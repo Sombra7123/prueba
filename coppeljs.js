a821a8359c1ae9a8f1e259478e4a080bf0ba1078
Sombra7123 <167376319+Sombra7123@users.noreply.github.com>
2024-04-25 01:45:45 -0600

    Add files via upload

---
+++ coppeljs.js
@@ -0,0 +1,9 @@
+alert('Bienvenido(a) a nuestra pagina web :)')
+var res=prompt("Alguna vez haz usado uno de los servicios de coppel?");
+if(res=="si")
+{
+    alert("Muy bien esperamos que lo siga haciendo")
+}
+else{
+    alert("Lo invitamos a que lo haga, no se arrepentira")
+}
